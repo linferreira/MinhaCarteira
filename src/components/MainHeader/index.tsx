ef095f89a49ee20a65076ff92d92cb31f5e17891
Lindsay Ferreira <eson@iouu.com.br>
2020-09-30 16:36:14 -0300

    feat: criar array de emojis personalizados

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,10 +1,24 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useMemo } from "react";
+
+import Emojis from "../../utils/emojis";
+
+import { Container, Profile, Welcome, UserName } from "./styles";
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * Emojis.length);
+    return Emojis[indice];
+  }, []);
+
   return (
     <Container>
-      <h2>MainHeader</h2>
+      <h2>Toogle</h2>
+
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+
+        <UserName>Lindsay</UserName>
+      </Profile>
     </Container>
   );
 };
