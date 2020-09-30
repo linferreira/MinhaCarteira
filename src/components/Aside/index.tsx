23ccc9cbedf04d9e2a53ba5d3f8cb5646b25633d
Lindsay Ferreira <eson@iouu.com.br>
2020-09-30 17:52:35 -0300

    feat: criar menu lateral

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,51 @@
 import React from "react";
-import { Container } from "./styles";
+
+import logo from "../../assets/logo.svg";
+
+import {
+  MdDashboard,
+  MdArrowUpward,
+  MdArrowDownward,
+  MdKeyboardTab,
+} from "react-icons/md";
+
+import {
+  Container,
+  Header,
+  LogoImg,
+  MenuContainer,
+  MenuItemLink,
+  Title,
+} from "./styles";
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h2>Aside</h2>
+      <Header>
+        <LogoImg src={logo} alt="Logo Minha Carteira" />
+        <Title>Minha Carteira</Title>
+      </Header>
+
+      <MenuContainer>
+        <MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+          Saídas
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdKeyboardTab /> Sair
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 };
