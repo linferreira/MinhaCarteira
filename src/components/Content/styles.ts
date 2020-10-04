c921bdd0c9df84e21db4123a9da396087e801e29
Lindsay Ferreira <eson@iouu.com.br>
2020-10-03 23:38:12 -0300

    feat: personalizando scrollbar

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -3,8 +3,24 @@ import styled from "styled-components";
 export const Container = styled.div`
   grid-area: CT;
   color: ${(props) => props.theme.colors.white};
-
   background-color: ${(props) => props.theme.colors.primary};
 
   padding: 25px;
+
+  height: calc(100vh - 70px);
+
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: ${(props) => props.theme.colors.tertiary};
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${(props) => props.theme.colors.secondary};
+  }
 `;
