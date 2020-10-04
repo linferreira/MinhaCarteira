7112972307f97a2529e93792ec9fc3f831604be6
Lindsay Ferreira <eson@iouu.com.br>
2020-10-03 23:56:13 -0300

    feat: criar filtros

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,93 +4,52 @@ import ContentHeader from "../../components/ContentHeader";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 import SelectInput from "../../components/SelectInput";
 
-import { Container, Content } from "./styles";
+import { Container, Content, Filter } from "./styles";
 
 const List: React.FC = () => {
-  const options = [
+  const months = [
     {
-      value: "Babi  ",
-      label: "Babi",
+      value: 7,
+      label: "Julho",
     },
     {
-      value: "Yuri",
-      label: "Yuri",
+      value: 8,
+      label: "Agosto",
     },
     {
-      value: "Marco",
-      label: "Marco",
+      value: 9,
+      label: "Setembro",
     },
   ];
+
+  const years = [
+    {
+      value: 2020,
+      label: 2020,
+    },
+    {
+      value: 2019,
+      label: 2019,
+    },
+    {
+      value: 2018,
+      label: 2018,
+    },
+  ];
+
   return (
     <Container>
       <ContentHeader title="Saída" lineColor="#e44c4e">
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
 
+      <Filter>
+        <button className="tag-filter tag-filter-recurrent">Recorrentes</button>
+        <button className="tag-filter tag-filter-eventual">Eventuais</button>
+      </Filter>
+
       <Content>
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="03/10/2020"
-          amount="R$ 125,50"
-        />
-
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="03/10/2020"
-          amount="R$ 125,50"
-        />
-
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="03/10/2020"
-          amount="R$ 125,50"
-        />
-
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="03/10/2020"
-          amount="R$ 125,50"
-        />
-
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="03/10/2020"
-          amount="R$ 125,50"
-        />
-
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="03/10/2020"
-          amount="R$ 125,50"
-        />
-
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="03/10/2020"
-          amount="R$ 125,50"
-        />
-
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="03/10/2020"
-          amount="R$ 125,50"
-        />
-
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="03/10/2020"
-          amount="R$ 125,50"
-        />
-
         <HistoryFinanceCard
           tagColor="#e44c4e"
           title="Conta de Luz"
