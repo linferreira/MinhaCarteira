e14e475d77dcc2349cddcc595fcb9a19bc7dbaf0
Lindsay Ferreira <eson@iouu.com.br>
2020-11-21 19:34:13 -0300

    feat: atualizar pagina de signin com autenticacao

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Container, Logo, Form, FormTitle } from "./styles";
 
@@ -7,7 +7,19 @@ import Button from "../../components/Button";
 
 import logoImg from "../../assets/logo.svg";
 
+import { useAuth } from "../../hooks/auth";
+
 const SignIn: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const { signIn } = useAuth();
+
+  function trySignIn(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    signIn(email, password);
+  }
+
   return (
     <Container>
       <Logo>
@@ -15,11 +27,21 @@ const SignIn: React.FC = () => {
         <h2>Minha Carteira</h2>
       </Logo>
 
-      <Form onSubmit={() => {}}>
+      <Form onSubmit={(e) => trySignIn(e)}>
         <FormTitle>Entrar</FormTitle>
 
-        <Input type="email" placeholder="email" required />
-        <Input type="password" placeholder="senha" required />
+        <Input
+          type="email"
+          placeholder="email"
+          onChange={({ target }) => setEmail(target.value)}
+          required
+        />
+        <Input
+          type="password"
+          placeholder="senha"
+          onChange={({ target }) => setPassword(target.value)}
+          required
+        />
 
         <Button type="submit">Acessar</Button>
       </Form>
