d263c4bcb25e17a0438dd697908b3783da185313
Lindsay Ferreira <eson@iouu.com.br>
2020-11-21 18:24:38 -0300

    feat: criar pagina de signin

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 
+import { Container, Logo, Form, FormTitle } from "./styles";
+
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+import logoImg from "../../assets/logo.svg";
+
 const SignIn: React.FC = () => {
   return (
-    <div>
-      <h1>Lindsay</h1>;<h2>SignIn</h2>
-    </div>
+    <Container>
+      <Logo>
+        <img src={logoImg} alt="Minha Carteira" />
+        <h2>Minha Carteira</h2>
+      </Logo>
+
+      <Form onSubmit={() => {}}>
+        <FormTitle>Entrar</FormTitle>
+
+        <Input type="email" placeholder="email" required />
+        <Input type="password" placeholder="senha" required />
+
+        <Button type="submit">Acessar</Button>
+      </Form>
+    </Container>
   );
 };
 
