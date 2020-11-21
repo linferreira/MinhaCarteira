d6825cadc870561d43ef3b364897f57b49b221a2
Lindsay Ferreira <eson@iouu.com.br>
2020-11-21 17:16:40 -0300

    feat: importar o hook themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 
+import { useTheme } from "./hooks/themes";
+
 import Routes from "./routes";
 
-import dark from "./styles/themes/dark";
-// import light from "./styles/themes/light";
-
 const App: React.FC = () => {
+  const { theme } = useTheme();
   return (
-    <ThemeProvider theme={dark}>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Routes />
     </ThemeProvider>
