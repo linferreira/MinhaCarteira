391fd795a6cb80aad862dab6246e0437435b0164
Lindsay Ferreira <eson@iouu.com.br>
2020-11-21 21:38:57 -0300

    feat: deixar o componente Aside responsivo

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -1,6 +1,14 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Container = styled.div`
+interface IContainerProps {
+  menuIsOpen: boolean;
+}
+
+interface IThemeFooterProps {
+  menuIsOpen: boolean;
+}
+
+export const Container = styled.div<IContainerProps>`
   grid-area: AS;
   color: ${(props) => props.theme.colors.white};
   background-color: ${(props) => props.theme.colors.secondary};
@@ -8,11 +16,36 @@ export const Container = styled.div`
   padding-left: 20px;
 
   border-right: 1px solid ${(props) => props.theme.colors.gray};
+
+  position: relative;
+
+  @media (max-width: 600px) {
+    padding-left: 16px;
+    position: fixed;
+    z-index: 2;
+
+    width: 170px;
+
+    height: ${(props) => (props.menuIsOpen ? "100vh" : "70px")};
+
+    overflow: hidden;
+
+    ${(props) =>
+      !props.menuIsOpen &&
+      css`
+        border: none;
+        border-bottom: 1px solid ${(props) => props.theme.colors.gray};
+      `};
+  }
 `;
 
 export const Title = styled.h3`
   color: ${(props) => props.theme.colors.white};
   margin-left: 10px;
+
+  @media (max-width: 600px) {
+    display: none;
+  }
 `;
 
 export const Header = styled.header`
@@ -24,6 +57,10 @@ export const Header = styled.header`
 export const LogoImg = styled.img`
   height: 40px;
   width: 40px;
+
+  @media (max-width: 600px) {
+    display: none;
+  }
 `;
 
 export const MenuContainer = styled.nav`
@@ -74,5 +111,42 @@ export const MenuItemButton = styled.button`
   > svg {
     font-size: 20px;
     margin-right: 7px;
+  }
+`;
+
+export const ToogleMenu = styled.button`
+  height: 40px;
+  width: 40px;
+
+  display: none;
+
+  color: ${(props) => props.theme.colors.white};
+
+  border-radius: 5px;
+
+  font-size: 22px;
+
+  background-color: ${(props) => props.theme.colors.warning};
+
+  transition: opacity 0.3s;
+
+  &hover {
+    opacity: 0.7;
+  }
+
+  @media (max-width: 600px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export const ThemeToogleFooter = styled.footer<IThemeFooterProps>`
+  display: none;
+  position: absolute;
+  bottom: 30px;
+
+  @media (max-width: 470px) {
+    display: ${(props) => (props.menuIsOpen ? "flex" : "none")};
   }
 `;
