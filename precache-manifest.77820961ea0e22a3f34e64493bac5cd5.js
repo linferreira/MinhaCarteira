527b938c1a46b83d921ac5b67a2b029aec635380
Lindsay Ferreira <eson@iouu.com.br>
2020-11-23 09:33:54 -0300

    Updates

---
+++ precache-manifest.77820961ea0e22a3f34e64493bac5cd5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d36ed6ab39ed549282fca59e31cb4ef",
+    "url": "/minha-carteira/index.html"
+  },
+  {
+    "revision": "39afb77106ede058b5e3",
+    "url": "/minha-carteira/static/js/2.7cb9780e.chunk.js"
+  },
+  {
+    "revision": "736f3ad4ee5990319c2a87119f7ebea6",
+    "url": "/minha-carteira/static/js/2.7cb9780e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e47bbcb21532816c599",
+    "url": "/minha-carteira/static/js/main.b245abcd.chunk.js"
+  },
+  {
+    "revision": "95629db2fea39e286ff9",
+    "url": "/minha-carteira/static/js/runtime-main.624565b8.js"
+  },
+  {
+    "revision": "81059ca205a82d7c4276c80c0a8d5568",
+    "url": "/minha-carteira/static/media/arrow-down.81059ca2.svg"
+  },
+  {
+    "revision": "ecbbd9cf6eb71246536590dd3a596f2b",
+    "url": "/minha-carteira/static/media/arrow-up.ecbbd9cf.svg"
+  },
+  {
+    "revision": "d56dac333231b66c2876ac37190de775",
+    "url": "/minha-carteira/static/media/dollar.d56dac33.svg"
+  },
+  {
+    "revision": "6ff3dc2c975a241cdb8f7509a50a782e",
+    "url": "/minha-carteira/static/media/grinning.6ff3dc2c.svg"
+  },
+  {
+    "revision": "eb82222585fd711a6e9a06700df496a1",
+    "url": "/minha-carteira/static/media/happy.eb822225.svg"
+  },
+  {
+    "revision": "7f611f0fa14dfd6718ccbf8a420a996e",
+    "url": "/minha-carteira/static/media/logo.7f611f0f.svg"
+  },
+  {
+    "revision": "f1b8a2f6cc80a1223f3d3ee242955a36",
+    "url": "/minha-carteira/static/media/ops.f1b8a2f6.svg"
+  },
+  {
+    "revision": "af596016322643905e591f48477a09d1",
+    "url": "/minha-carteira/static/media/sad.af596016.svg"
+  }
+]);
